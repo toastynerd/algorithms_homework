377c982760b90dc6b18d1ef1d25cae7effd3f74c
tylermorgan86 <tylermorgan86@gmail.com>
2013-07-14 10:53:19 -0700

    non working solution only finds inversions next to each other

--- inversion_counter.rb
+++ inversion_counter.rb
@@ -1,14 +1,19 @@
 #!/usr/bin/env ruby
 def get_inversions(array)
-  unless array.size == 2
+  unless array.size <= 2
     get_inversions(array[0..(array.size/2)])
     get_inversions(array[(array.size/2+1)..array.size])
   else
-    puts "blerg"
+    unless array.size < 2
+      if array[0] > array[1]
+        @num_inversions += 1
+      end
+    end
   end
 end
 
 @my_numbers = ARGF.readlines
-@num_inversion = 0
+@num_inversions = 0
 get_inversions(@my_numbers)
+puts @num_inversions
 
